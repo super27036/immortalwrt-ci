a5f812d9b1da9960609f3a9752141bfa5ae773f3
super27036 <144255894+super27036@users.noreply.github.com>
2024-09-01 19:36:56 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,7 +10,24 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
-# Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+#-------------#
+### 删除frp ###
+rm -rf feeds/luci/applications/luci-app-frpc
+rm -rf feeds/luci/applications/luci-app-frps
 
-#sed -i 's/192.168.123.5/192.168.50.5/g' package/base-files/files/bin/config_generate
+# 从 coolsnowwolf 仓库中获取 luci-app-frpc 和 luci-app-frps 应用
+git clone --depth 1 https://github.com/coolsnowwolf/luci.git /tmp/luci
+
+# 复制 luci-app-frpc 和 luci-app-frps 应用到 feeds/luci/applications/
+cp -rf /tmp/luci/applications/luci-app-frpc feeds/luci/applications/
+cp -rf /tmp/luci/applications/luci-app-frps feeds/luci/applications/
+
+# 删除临时目录
+rm -rf /tmp/luci
+#-------------#
+
+# Modify default hosename
+sed -i 's/ImmortalWrt/SUPERouter/g' package/base-files/files/bin/config_generate
+# Password
+# sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/$1$S2TRFyMU$E8fE0RRKR0jNadn3YLrSQ0:18690:0:99999:7:::/g' package/lean/default-settings/files/zzz-default-settings
+
